14a8392a7173cdfc0800e89bb55a6b2d14544ea2
Sharath2205 <pendyalasharath321@gmail.com>
2024-07-18 21:46:12 +0530

    order page added

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,47 +1,58 @@
-/* eslint-disable no-unused-vars */
 import { useContext } from "react";
 
 import { StoreContext } from "../../context/StoreContext";
 
 import "./Cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getCartTotal } =
     useContext(StoreContext);
 
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div className="cart-items">
-        <div className="cart-items-title">
-          <p>Items</p>
-          <p>Title</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p>Remove</p>
-        </div>
-        <br />
-        <hr />
-        {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
-            return (
-              <div key={index}>
-                <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>${item.price}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
-                  <p className="cross" onClick={() => removeFromCart(item._id)}>
-                    X
-                  </p>
-                </div>
-                <hr />
-              </div>
-            );
-          }
-          return null;
-        })}
+        {Object.keys(cartItems).length !== 0 ? (
+          <>
+            <div className="cart-items-title">
+              <p>Items</p>
+              <p>Title</p>
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Total</p>
+              <p>Remove</p>
+            </div>
+            <br />
+            <hr />
+            {food_list.map((item, index) => {
+              if (cartItems[item._id] > 0) {
+                return (
+                  <div key={index}>
+                    <div className="cart-items-title cart-items-item">
+                      <img src={item.image} alt="" />
+                      <p>{item.name}</p>
+                      <p>${item.price}</p>
+                      <p>{cartItems[item._id]}</p>
+                      <p>${item.price * cartItems[item._id]}</p>
+                      <p
+                        className="cross"
+                        onClick={() => removeFromCart(item._id)}
+                      >
+                        X
+                      </p>
+                    </div>
+                    <hr />
+                  </div>
+                );
+              }
+              return null;
+            })}
+          </>
+        ) : (
+          <div className="empty-cart">Your cart is empty</div>
+        )}
       </div>
 
       <div className="cart-bottom">
@@ -55,15 +66,17 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery fee</p>
-              <p>${2}</p>
+              <p>${getCartTotal() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getCartTotal() + 2}</b>
+              <b>${getCartTotal() != 0 ? getCartTotal() + 2 : 0}</b>
             </div>
           </div>
-          <button>Proceed to checkout</button>
+          <button onClick={() => navigate("/order")}>
+            Proceed to checkout
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
